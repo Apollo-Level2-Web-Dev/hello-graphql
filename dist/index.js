d9bcbb9fe67a5971c08383f947af567f6d9d8fb9
fahimahammed <fahimahammed.cse@gmail.com>
2023-10-30 22:15:26 +0600

    init

---
+++ dist/index.js
@@ -0,0 +1,32 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { db } from './db.js';
+const typeDefs = `#graphql
+
+    type Product {
+        id: ID!
+        name: String
+        image: String
+        description: String
+        price: Float
+        quantity: Int
+        onStock: Boolean
+        category: String
+    }
+  type Query {
+    products: [Product]
+  }
+`;
+const resolvers = {
+    Query: {
+        products: () => db.products
+    },
+};
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+});
+console.log(`🚀  Server ready at: ${url}`);
